651655e64c38ffaa6dbb5ba6b0744732ca85ead1
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-10-09 22:36:14 -0300

    Configurações Iniciais e Layout Principal (RV)
    Inserindo Dependencias e Plugins Necessários para o Projeto, Criando e Instanciando RecyclerView no XML, e Adicionando Permissões de Internet no Manifest.

--- app/src/main/java/com/example/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/example/applicationworkapi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerViewProducts: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerViewProducts = rvProducts
     }
 }
